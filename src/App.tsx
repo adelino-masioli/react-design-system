a593f19c274115d5a1c3a92c27f0fa862a0b9f88
Adelino Masioli <adelinomasioli@Adelinos-MBP.home>
2022-10-16 10:49:11 +0100

    Page login with Storybook components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
-import './styles/global.css'
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Logo } from './Logo';
+import './styles/global.css';
 
 export function App() {
   return (
-    <h1 className="text-3xl font-bold text-violet-500">Hello World</h1>
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100'>
+     <header className='flex flex-col items-center'>
+      <Logo/>
+        <Heading size='lg' className='mt-4'>Ignite Lab</Heading>
+        <Text size='lg' className='text-gray-400 mt-1'>Design System</Text>
+      </header>
+
+      <form className="flex flex-col items-stretch w-full max-w-[400px] mt-10 gap-4">
+        <label htmlFor="email" className='flex flex-col gap-3'>
+          <Text className='font-semibol'>Email address</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input placeholder="email@email.com" />
+          </TextInput.Root>
+        </label>
+        <label htmlFor="password" className='flex flex-col gap-3'>
+          <Text className='font-semibol'>Your password</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input placeholder="********" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember"  className='flex items-center gap-2'>
+          <Checkbox id='remember'/>
+          <Text className='font-semibol'>Remember-me</Text>
+        </label>
+
+        <Button type='submit' className='mt-4'>Access dashboard</Button>
+      </form>
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text asChild size='sm'>
+          <a href="#" className='text-gray-400 underline hover:text-gray-200 transition-colors'>Forgot your password?</a>
+        </Text>
+        <Text asChild size='sm'>
+          <a href="#" className='text-gray-400 underline hover:text-gray-200 transition-colors'>Don't have an account yet? Sign Up</a>
+        </Text>
+      </footer>
+    </div>
   )
 }
